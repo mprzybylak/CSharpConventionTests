6bd33190765ae56fc0b6f93625ec1a01b5fe5c63
Michał Przybylak <michal.przybylak@gmail.com>
2017-09-10 22:53:47 +0200

    sorted sketches of methods

--- minefieldtests/Test.cs
+++ minefieldtests/Test.cs
@@ -9,10 +9,33 @@ namespace minefieldtests
 		[Test]
 		public void TestCase()
 		{
-			var a = typeof(IPricingStrategy).Assembly.ExportedTypes; // types that are visible outside of solution
-			typeof(IPricingStrategy).Assembly.GetCustomAttributes(true); // custom attrybuty
-			typeof(IPricingStrategy).Assembly.GetTypes(); // types from assembly
 
+		}
+
+		private void assemblyUsefulMethods()
+		{
+			var a = typeof(IPricingStrategy).Assembly.ExportedTypes; // types that are visible outside of solution
+			typeof(IPricingStrategy).Assembly.GetTypes(); // types from assembly
+		}
+
+		private void methodsForFiltering()
+		{
+			var d = typeof(IPricingStrategy).IsAbstract; // is abstract class
+			var e = typeof(IPricingStrategy).IsEnum; // is enum
+			var f = typeof(IPricingStrategy).IsClass; // is class
+
+			var g = typeof(IPricingStrategy).IsInterface; // is interface
+			var h = typeof(IPricingStrategy).IsValueType; // is value type
+			typeof(IPricingStrategy).IsSubclassOf(typeof(object)); // true if class derives from type passed as an argument
+			typeof(IPricingStrategy).IsAssignableFrom(typeof(object)); // true if passed class can be assigned to reference of this class
+			typeof(IPricingStrategy).IsInstanceOfType(new object()); // true if passed object can be assigned to reference ot this class 
+
+			var l = typeof(IPricingStrategy).Namespace; // namespace of type
+			var m = typeof(IPricingStrategy).Name; // name of type
+		}
+
+		private void methodsToUseAfterFiltering()
+		{
 			var b = typeof(IPricingStrategy).Attributes; // get attributes for current type
 			var c = typeof(IPricingStrategy).BaseType; // get base type for current type
 			typeof(IPricingStrategy).GetConstructors(); // get all constructor
@@ -21,21 +44,7 @@ namespace minefieldtests
 			typeof(IPricingStrategy).GetInterfaces(); // get all implemented interfaces
 			typeof(IPricingStrategy).GetMembers(); // get public members
 			typeof(IPricingStrategy).GetMethods(); // get all methods
-			typeof(IPricingStrategy).GetProperties();
-
-			var d = typeof(IPricingStrategy).IsAbstract;
-			var e = typeof(IPricingStrategy).IsEnum;
-			var f = typeof(IPricingStrategy).IsClass;
-
-			var g = typeof(IPricingStrategy).IsInterface;
-			var h = typeof(IPricingStrategy).IsValueType;
-			typeof(IPricingStrategy).IsSubclassOf(typeof(object));
-			typeof(IPricingStrategy).IsAssignableFrom(typeof(object));
-			typeof(IPricingStrategy).IsInstanceOfType(typeof(object));
-
-			var l = typeof(IPricingStrategy).Namespace;
-			var m = typeof(IPricingStrategy).Name;
-
+			typeof(IPricingStrategy).GetProperties(); // get all properties
 		}
 	}
 }
