5679dcbb3d4e5b8507ed2a961e98b376182a9d60
Michał Przybylak <michal.przybylak@gmail.com>
2017-09-10 22:44:17 +0200

    sketches of useful methods to use

--- minefieldtests/Test.cs
+++ minefieldtests/Test.cs
@@ -1,6 +1,6 @@
 ﻿using NUnit.Framework;
-using System;
 using minefield.ECommerce.Pricing;
+using System.Linq;
 
 namespace minefieldtests
 {
@@ -9,6 +9,32 @@ namespace minefieldtests
 		[Test]
 		public void TestCase()
 		{
+			var a = typeof(IPricingStrategy).Assembly.ExportedTypes; // types that are visible outside of solution
+			typeof(IPricingStrategy).Assembly.GetCustomAttributes(true); // custom attrybuty
+			typeof(IPricingStrategy).Assembly.GetTypes(); // types from assembly
+
+			var b = typeof(IPricingStrategy).Attributes; // get attributes for current type
+			var c = typeof(IPricingStrategy).BaseType; // get base type for current type
+			typeof(IPricingStrategy).GetConstructors(); // get all constructor
+			typeof(IPricingStrategy).GetEnumNames(); // get all names defined in this enum - exception if not enum
+			typeof(IPricingStrategy).GetEvents(); // get all events defined in type
+			typeof(IPricingStrategy).GetInterfaces(); // get all implemented interfaces
+			typeof(IPricingStrategy).GetMembers(); // get public members
+			typeof(IPricingStrategy).GetMethods(); // get all methods
+			typeof(IPricingStrategy).GetProperties();
+
+			var d = typeof(IPricingStrategy).IsAbstract;
+			var e = typeof(IPricingStrategy).IsEnum;
+			var f = typeof(IPricingStrategy).IsClass;
+
+			var g = typeof(IPricingStrategy).IsInterface;
+			var h = typeof(IPricingStrategy).IsValueType;
+			typeof(IPricingStrategy).IsSubclassOf(typeof(object));
+			typeof(IPricingStrategy).IsAssignableFrom(typeof(object));
+			typeof(IPricingStrategy).IsInstanceOfType(typeof(object));
+
+			var l = typeof(IPricingStrategy).Namespace;
+			var m = typeof(IPricingStrategy).Name;
 
 		}
 	}
