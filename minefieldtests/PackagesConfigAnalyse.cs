8b55aacab5df9de0c6f84194ab644446b5d0a1b0
Michał Przybylak <michal.przybylak@gmail.com>
2017-09-16 20:44:39 +0200

    checking allowed versions inside packages.config

---
+++ minefieldtests/PackagesConfigAnalyse.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using NUnit.Framework;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace minefieldtests
+{
+    public class PackagesConfigAnalyse
+    {
+
+        [Test]
+        public void AllowedVersionConstraintsIsNotViolated() {
+
+            var count = GetFiles("minefieldtests")
+                .Where(n => n.Equals("packages.config"))
+                .Select(n => File.ReadAllText(n))
+                .Where(f => !Regex.IsMatch(f, "id=\"NUnit\".*allowedVersions=\".2,3.\""))
+                .Count();
+
+            Assert.AreEqual(0, count);
+        }
+
+		public List<string> GetFiles(string path)
+		{
+			var d = new DirectoryInfo(Directory.GetCurrentDirectory());
+			while (d.EnumerateFiles("*.sln").Any() == false)
+			{
+				d = d.Parent;
+			}
+			var fullPath = Path.Combine(d.FullName, path);
+
+			return Directory.EnumerateFiles(fullPath)
+							.ToList();
+		}
+    }
+}
