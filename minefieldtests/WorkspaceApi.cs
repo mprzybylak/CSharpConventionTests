af242b639fba8386c9170f234fba8c6440a86546
Michał Przybylak <michal.przybylak@gmail.com>
2017-09-24 22:49:17 +0200

    test file for workspace api

---
+++ minefieldtests/WorkspaceApi.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace minefieldtests
+{
+
+    [Test]
+    public void messingAround() {
+        
+    }
+
+	private string GetSolution()
+	{
+		var d = new DirectoryInfo(Directory.GetCurrentDirectory());
+		while (d.EnumerateFiles("*.sln").Any() == false)
+		{
+			d = d.Parent;
+		}
+		return d.FullName;
+	}
+}
